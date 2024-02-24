012116c1daa4f0bf8ab8cefa0d93c0c4f7a15b19
AnaghaCS <anaghasasidharan38@gmail.com>
2024-02-24 09:33:14 +0000

    models

--- hotel_management_system/models.py
+++ hotel_management_system/models.py
@@ -13,6 +13,8 @@ class User(AbstractUser):
     base_role = Role.ADMIN
 
     role = models.CharField(max_length=50, choices=Role.choices)
+    approved = models.BooleanField(default=False)
+
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -58,9 +60,16 @@ class HotelManager(BaseUserManager):
         results = super().get_queryset(*args, **kwargs)
         return results.filter(role=User.Role.HOTEL)
 
-
 class Hotel(User):
     base_role = User.Role.HOTEL
+    hotel_name = models.CharField(max_length = 20)      
+    address = models.CharField(blank=True, max_length=100)
+    place = models.CharField(blank=True, max_length=20)
+    district = models.CharField(blank=True, max_length=20)
+    state = models.CharField(blank=True, max_length=20)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length = 12, unique = True)
+    photo = models.ImageField(upload_to = 'media', blank = True, null = True)
 
     student = HotelManager()
 
@@ -70,7 +79,7 @@ class Hotel(User):
     def welcome(self):
         return "Only for Hotels"
     
-    
+
 
 class HotelProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -83,7 +92,6 @@ class HotelProfile(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     phone = models.CharField(max_length = 12, unique = True)
     photo = models.ImageField(upload_to = 'media', blank = True, null = True)
-    approved = models.BooleanField(default=False)
 
 
 # @receiver(post_save, sender=User)
@@ -98,3 +106,6 @@ def create_user_profile(sender, instance, created, **kwargs):
             CustomerProfile.objects.create(user=instance)
         elif instance.role == User.Role.HOTEL:
             HotelProfile.objects.create(user=instance)
+
+class Staff(User):
+    base_role = User.Role.HOTEL
